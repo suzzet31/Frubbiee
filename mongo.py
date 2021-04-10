0301525d720fc5906d41d50447dbc343f1f06de2
suzzet31 <suezett31@gmail.com>
2021-04-10 20:48:42 +0000

    modified app.py

---
+++ mongo.py
@@ -0,0 +1,27 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "frubbie_drinks"
+COLLECTION = "users"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+
+for doc in documents:
+    print(doc)
