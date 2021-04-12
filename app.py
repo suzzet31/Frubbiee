670a1c44e31b738b1d7919271cc261512a834c50
suzzet31 <suezett31@gmail.com>
2021-04-12 17:08:55 +0000

    connect flask to Mongodb

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
